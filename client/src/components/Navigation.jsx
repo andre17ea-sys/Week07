09ddb771dc849027cb48d94550780e8e988103e0
ANDREEA TOMA <andre17ea@gmail.com>
2025-10-20 17:21:30 +0100

    change frontend fetch urls to render backend

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 export default function Navigation() {
   return (
-    <nav>
+    <nav className="navigation">
       <Link to="/">Home</Link> | <Link to="/events">Events</Link>
     </nav>
   );
